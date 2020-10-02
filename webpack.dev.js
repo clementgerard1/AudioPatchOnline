c3e1af8eaee75c1f4a15a74eb9a059d79c02b558
Clément Gérard <gerard.clement25@gmail.com>
2020-10-02 12:38:07 +0200

    webpack init setup

---
+++ webpack.dev.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+	mode:'development',
+	watch : true,
+	entry: {
+  	bundle : './src/app/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dev'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: [
+    	path.join(__dirname, 'dev'),
+    	path.join(__dirname, 'public')
+    ],
+    compress: true,
+    port: 3000
+  },
+  module: {
+	  rules: [
+	    {
+	      test: /\.m?js$/,
+	      exclude: /(node_modules|bower_components)/,
+	      use: {
+	        loader: 'babel-loader',
+	        options: {
+	          presets: ['@babel/preset-env'],
+	          plugins: [
+	          	'@babel/plugin-proposal-class-properties', 
+	          	"@babel/plugin-proposal-private-methods"
+	          ],
+	        }
+	      }
+	    }
+	  ]
+	}
+};
---
+++ webpack.prod.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+	mode:'production',
+	watch : false,
+	entry: {
+  	bundle : './src/app/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+	  rules: [
+	    {
+	      test: /\.m?js$/,
+	      exclude: /(node_modules|bower_components)/,
+	      use: {
+	        loader: 'babel-loader',
+	        options: {
+	          presets: ['@babel/preset-env'],
+	          plugins: [
+	          	'@babel/plugin-proposal-class-properties', 
+	          	"@babel/plugin-proposal-private-methods"
+	          ],
+	        }
+	      }
+	    }
+	  ]
+	}
+};
